8df1057f16cab1bf6077e7ab5813a194a33b0417
Ciszkowska <ciszkowska.magdalena@gmail.com>
2024-05-12 12:22:37 +0100

    added missing ` to humidity and wind element

--- src/home.js
+++ src/home.js
@@ -6,31 +6,39 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector("#windSpeed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
-  let iconElement = document.querySelector("#icon");  
-  
-  
-  icon.innerHTML =`<img src="${response.data.condition.icon_url} class="weather-icon"/>`;
+  let iconElement = document.querySelector("#icon");
+
+  icon.innerHTML = `<img src="${response.data.condition.icon_url} class="weather-icon"/>`;
 
   timeElement.innerHTML = formatDate();
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = response.data.temperature.current;
   descritptionElement.innerHTML = response.data.condition.description;
-  humidityElement.innerHTML = ${response.data.temperature.humidity} %;
-  windSpeedElement.innerHTML = ${response.data.wind.speed} km/h;  
+  humidityElement.innerHTML = `${response.data.temperature.humidity} %`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
 }
 
-function formatDate(date){
-
+function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
-  let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]; 
-  
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
   let day = days[date.getDay()];
-  if (minutes <10){
-    minutes = `0 ${minutes}`
+
+  if (minutes < 10) {
+    minutes = `0 ${minutes}`;
   }
-  if (hours <10){
-    minutes = `0${hours}`
+
+  if (hours < 10) {
+    minutes = `0${hours}`;
   }
   return `${day} ${hours}: ${minutes}`;
 }
