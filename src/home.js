928c5b23b105836fdb14f37992e67e7f028cdf7c
Ciszkowska <ciszkowska.magdalena@gmail.com>
2024-05-12 12:29:17 +0100

    Update home.js

--- src/home.js
+++ src/home.js
@@ -8,7 +8,7 @@ function refreshWeather(response) {
   let date = new Date(response.data.time * 1000);
   let iconElement = document.querySelector("#icon");
 
-  icon.innerHTML = `<img src="${response.data.condition.icon_url} class="weather-icon"/>`;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url} class="weather-icon"/>`;
 
   timeElement.innerHTML = formatDate();
   cityElement.innerHTML = response.data.city;
