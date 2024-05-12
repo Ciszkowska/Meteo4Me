7061b7c1b8618782cd20cb27c6360f0d5300e305
Ciszkowska <ciszkowska.magdalena@gmail.com>
2024-05-12 12:11:13 +0100

    replace emoji with icon

--- src/home.js
+++ src/home.js
@@ -5,13 +5,34 @@ function refreshWeather(response) {
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#windSpeed");
   let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");  
+  
+  
+  icon.innerHTML =`<img src="${response.data.condition.icon_url} class="weather-icon"/>`;
 
-  timeElement.innerHTML = 2244422 
+  timeElement.innerHTML = formatDate();
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = response.data.temperature.current;
   descritptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = ${response.data.temperature.humidity} %;
   windSpeedElement.innerHTML = ${response.data.wind.speed} km/h;  
+}
+
+function formatDate(date){
+
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]; 
+  
+  let day = days[date.getDay()];
+  if (minutes <10){
+    minutes = `0 ${minutes}`
+  }
+  if (hours <10){
+    minutes = `0${hours}`
+  }
+  return `${day} ${hours}: ${minutes}`;
 }
 
 function searchCity(city) {
