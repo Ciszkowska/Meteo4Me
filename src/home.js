9dae1af3630d372506400665643d1fe2949492b6
Ciszkowska <ciszkowska.magdalena@gmail.com>
2024-05-12 20:42:38 +0100

    updated netify link

--- src/home.js
+++ src/home.js
@@ -6,7 +6,7 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector("#windSpeed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
-  let iconElement = document.querySelector("#icon");
+  let iconElement = document.querySelector(`#icon`);
 
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url} class="weather-icon"/>`;
 
